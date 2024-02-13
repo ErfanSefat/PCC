c547e39dc4e7d67e83fab6566566e1a3a469d703
Erfan <erfansefat@outlook.com>
2024-02-13 13:23:12 +0330

    spotify card is done

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-const scrollers = document.querySelectorAll(".scroller");
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+const URL = "https://erfansefat.github.io/PCC/spotifycard.json";
+const musiccard = document.createElement("div");
+const SpotiScroll1 = document.getElementById("SpotiScroll1");
+const SpotiScroll2 = document.getElementById("SpotiScroll2");
+musiccard.className = "musiccard";
+let i = 0;
+async function loadData() {
+  const items = await fetch(URL);
+  const data = items.json();
+  return data;
+}
+async function loadItems() {
+  const items = await loadData();
+  items.albums.forEach((item) => {
+    const duplicatedItem = musiccard.cloneNode(true);
+    duplicatedItem.style.backgroundImage = `URL("./pics/${item.item_img}.png")`;
+    duplicatedItem.alt = `${item.item_name}`;
+    if(i < 5){SpotiScroll1.appendChild(duplicatedItem);}
+    else{SpotiScroll2.appendChild(duplicatedItem);}
+    i++;
+  });
   addAnimation();
 }
-function addAnimation() {
+const scrollers = document.querySelectorAll(".scroller");
+// if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+//   addAnimation();
+// }
+async function addAnimation() {
   scrollers.forEach((scroller) => {
     scroller.setAttribute("data-animated", true);
     const scrollerInner = scroller.querySelector(".scrollerinner");
@@ -15,3 +38,4 @@ function addAnimation() {
     });
   });
 }
+loadItems();
